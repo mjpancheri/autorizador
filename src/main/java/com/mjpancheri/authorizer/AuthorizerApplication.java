a136330467fbec7c3d8f3f129a5eb40d67f4be5a
Marcio Pancheri <mjpancheri@gmail.com>
2022-12-17 16:19:56 -0300

    chore: rename package and class

--- src/main/java/com/mjpancheri/authorizer/AuthorizerApplication.java
+++ src/main/java/com/mjpancheri/authorizer/AuthorizerApplication.java
@@ -1,13 +1,13 @@
-package com.mjpancheri.autorizador;
+package com.mjpancheri.authorizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class AutorizadorApplication {
+public class AuthorizerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AutorizadorApplication.class, args);
+		SpringApplication.run(AuthorizerApplication.class, args);
 	}
-
 }
