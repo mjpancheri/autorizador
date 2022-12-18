27e42d40ae73728847488b32f28954401ae7b0ae
Marcio Pancheri <mjpancheri@gmail.com>
2022-12-18 20:02:16 -0300

    feat: add payload to exception

--- src/main/java/com/mjpancheri/authorizer/common/exception/CardNumberConflictException.java
+++ src/main/java/com/mjpancheri/authorizer/common/exception/CardNumberConflictException.java
@@ -1,5 +1,11 @@
 package com.mjpancheri.authorizer.common.exception;
 
-public class CardNumberConflictException extends RuntimeException {
+import com.mjpancheri.authorizer.cards.application.rest.dto.CreateCardDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+public class CardNumberConflictException extends RuntimeException {
+  private final CreateCardDto payload;
 }
