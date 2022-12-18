491f7b31929b26b3d6f179c0a6af5a85c90d8aca
Marcio Pancheri <mjpancheri@gmail.com>
2022-12-18 20:07:01 -0300

    fix: add annotations

--- src/main/java/com/mjpancheri/authorizer/cards/application/rest/CardController.java
+++ src/main/java/com/mjpancheri/authorizer/cards/application/rest/CardController.java
@@ -3,10 +3,14 @@ package com.mjpancheri.authorizer.cards.application.rest;
 import com.mjpancheri.authorizer.cards.application.rest.dto.CreateCardDto;
 import com.mjpancheri.authorizer.cards.application.rest.swagger.api.CardApi;
 import com.mjpancheri.authorizer.cards.application.service.CardService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,14 +25,14 @@ public class CardController implements CardApi {
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   @Override
-  public CreateCardDto create(CreateCardDto createCardRequest) {
+  public CreateCardDto create(@RequestBody @Validated CreateCardDto createCardRequest) {
     return cardService.createCard(createCardRequest);
   }
 
   @GetMapping("/{cardNumber}")
   @ResponseStatus(HttpStatus.OK)
   @Override
-  public Double balance(String cardNumber) {
+  public Double balance(@PathVariable("cardNumber") @Valid String cardNumber) {
     return cardService.getBalance(cardNumber);
   }
 }
