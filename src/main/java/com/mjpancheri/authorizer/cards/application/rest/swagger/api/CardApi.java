b55a71ef8bb52651b7c7f35637c93282a20d672a
Marcio Pancheri <mjpancheri@gmail.com>
2022-12-18 19:58:42 -0300

    config: add config to documentation

--- src/main/java/com/mjpancheri/authorizer/cards/application/rest/swagger/api/CardApi.java
+++ src/main/java/com/mjpancheri/authorizer/cards/application/rest/swagger/api/CardApi.java
@@ -2,13 +2,10 @@ package com.mjpancheri.authorizer.cards.application.rest.swagger.api;
 
 import com.mjpancheri.authorizer.cards.application.rest.dto.CreateCardDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.IOException;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Card Api")
 public interface CardApi {
@@ -16,15 +13,17 @@ public interface CardApi {
   @Operation(summary = "Cria um novo cartão com saldo inicial de R$ 500,00.")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "201", description = "Cartão criado com sucesso."),
+      @ApiResponse(responseCode = "400", description = "Dados inválidos."),
       @ApiResponse(responseCode = "422", description = "Cartão já existe.")
   })
-  CreateCardDto create(@RequestBody @Validated CreateCardDto createCardRequest);
+  CreateCardDto create(@RequestBody CreateCardDto createCardRequest);
 
   @Operation(summary = "Verifica o saldo do cartão")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Cartão encotrado."),
+      @ApiResponse(responseCode = "400", description = "Dados inválidos."),
       @ApiResponse(responseCode = "404", description = "Cartão inexiste.")
   })
-  Double balance(@PathVariable String cardNumber);
+  Double balance( String cardNumber);
 
 }
