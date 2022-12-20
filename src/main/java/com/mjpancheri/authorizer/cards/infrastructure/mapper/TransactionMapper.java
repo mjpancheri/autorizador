a51177186509b8112df6e81b718bde53cbca2712
Marcio Pancheri <mjpancheri@gmail.com>
2022-12-20 09:59:33 -0300

    feat: add mapper to convert dto to transactions

---
+++ src/main/java/com/mjpancheri/authorizer/cards/infrastructure/mapper/TransactionMapper.java
@@ -0,0 +1,22 @@
+package com.mjpancheri.authorizer.cards.infrastructure.mapper;
+
+import com.mjpancheri.authorizer.cards.domain.Card;
+import com.mjpancheri.authorizer.cards.domain.Transaction;
+import java.math.BigDecimal;
+
+public class TransactionMapper {
+
+  private TransactionMapper(){}
+  
+  public static Transaction convertToTransaction(BigDecimal amount, Card card) {
+    if (amount == null || card == null) {
+      return null;
+    }
+
+    return Transaction.builder()
+        .amount(amount)
+        .card(card)
+        .build();
+  }
+
+}
