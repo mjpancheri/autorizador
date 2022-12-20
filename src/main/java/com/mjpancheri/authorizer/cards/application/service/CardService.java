ca9f8abb67c26d743f76fe89137f87ac1c74a077
Marcio Pancheri <mjpancheri@gmail.com>
2022-12-20 09:55:26 -0300

    refactor: add methods to support transactions

--- src/main/java/com/mjpancheri/authorizer/cards/application/service/CardService.java
+++ src/main/java/com/mjpancheri/authorizer/cards/application/service/CardService.java
@@ -4,10 +4,13 @@ import com.mjpancheri.authorizer.cards.application.rest.dto.CreateCardDto;
 import com.mjpancheri.authorizer.cards.domain.Card;
 import com.mjpancheri.authorizer.cards.domain.repository.CardRepository;
 import com.mjpancheri.authorizer.cards.infrastructure.mapper.CardMapper;
+import com.mjpancheri.authorizer.common.exception.CardIncorrectException;
 import com.mjpancheri.authorizer.common.exception.CardNotFoundException;
 import com.mjpancheri.authorizer.common.exception.CardNumberConflictException;
+import java.math.BigDecimal;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,7 +24,7 @@ public class CardService {
     if (cardRepository.existsByNumber(createCardDto.getNumeroCartao())) {
       throw new CardNumberConflictException(createCardDto);
     }
-    cardRepository.save(CardMapper.createCardDtoToCard(createCardDto));
+    cardRepository.save(CardMapper.convertToCard(createCardDto));
     return createCardDto;
   }
 
@@ -30,5 +33,25 @@ public class CardService {
         .orElseThrow(CardNotFoundException::new);
 
     return card.getBalance().doubleValue();
+  }
+
+  public void updateCardBalance(Card card, BigDecimal amount) {
+    var newBalance = card.getBalance().subtract(amount);
+    card.setBalance(newBalance);
+    cardRepository.save(card);
+  }
+
+  public Card getCard(String cardNumber) {
+    return cardRepository.findByNumber(cardNumber)
+        .orElseThrow(CardIncorrectException::new);
+  }
+
+  public boolean validCardPassword(Card card, String password) {
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    if (card == null || password == null) {
+      return false;
+    }
+
+    return bCryptPasswordEncoder.matches(password, card.getPassword());
   }
 }
