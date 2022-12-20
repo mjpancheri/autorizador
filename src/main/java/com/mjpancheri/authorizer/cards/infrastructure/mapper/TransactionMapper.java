f2f32fd4402a8d3fd7e1688d0941ffe5031ccca4
Marcio Pancheri <mjpancheri@gmail.com>
2022-12-20 10:02:07 -0300

    chore: organize code

--- src/main/java/com/mjpancheri/authorizer/cards/infrastructure/mapper/TransactionMapper.java
+++ src/main/java/com/mjpancheri/authorizer/cards/infrastructure/mapper/TransactionMapper.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 public class TransactionMapper {
 
   private TransactionMapper(){}
-  
+
   public static Transaction convertToTransaction(BigDecimal amount, Card card) {
     if (amount == null || card == null) {
       return null;
