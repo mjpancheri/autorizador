778b8547c4e45cb5f80c83ddc2531ef43af1ce62
Marcio Pancheri <mjpancheri@gmail.com>
2022-12-20 09:52:13 -0300

    feat: add constants to transaction and builders

--- src/main/java/com/mjpancheri/authorizer/common/util/CustomUtils.java
+++ src/main/java/com/mjpancheri/authorizer/common/util/CustomUtils.java
@@ -5,6 +5,17 @@ import com.mjpancheri.authorizer.common.exception.ConvertToJsonException;
 
 public class CustomUtils {
 
+  public static final String TRANSACTION_OK = "OK";
+  public static final String TRANSACTION_INSUFFICIENT_BALANCE = "SALDO_INSUFICIENTE";
+  public static final String TRANSACTION_INCORRECT_PASSWORD = "SENHA_INVALIDA";
+  public static final String TRANSACTION_INCORRECT_CARD = "CARTAO_INEXISTENTE";
+  public static final String CORRECT_CARD_NUMBER_MASK = "################";
+  public static final String INCORRECT_CARD_NUMBER_MASK = "########";
+  public static final String INVALID_CARD_NUMBER_MASK = "????????????????";
+  public static final String CORRECT_PASSWORD_MASK = "####";
+  public static final String INCORRECT_PASSWORD_MASK = "##";
+  public static final String INVALID_PASSWORD_MASK = "????";
+
   private CustomUtils(){}
 
   public static String asJsonString(final Object obj) {
